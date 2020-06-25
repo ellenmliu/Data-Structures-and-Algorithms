31cbcf33e5382c2e82ac05a3d2ef4c948b632f34
Ellen Liu <ellenmliu@Ellens-MacBook-Pro.local>
2020-06-24 21:43:53 -0700

    Finish task 1 with analysis

--- Unscramble CS Problems/Task1.py
+++ Unscramble CS Problems/Task1.py
@@ -14,7 +14,19 @@ with open('calls.csv', 'r') as f:
 
 """
 TASK 1:
-How many different telephone numbers are there in the records? 
+How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+phoneNumbers = set()
+
+for index in range(0, len(texts) - 1):
+    phoneNumbers.add(texts[index][0])
+    phoneNumbers.add(texts[index][1])
+
+for index in range(0, len(calls) - 1):
+    phoneNumbers.add(calls[index][0])
+    phoneNumbers.add(calls[index][1])
+
+print('There are {} different telephone numbers in the records.'.format(len(phoneNumbers)))
