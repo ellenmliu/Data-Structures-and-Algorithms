6876a9e05cee5706d6bf795d3f4392d037a97e02
Ellen Liu <ellenmliu@Ellens-MacBook-Pro.local>
2020-06-24 23:41:34 -0700

    Finish task 2 with analysis

--- Unscramble CS Problems/Task2.py
+++ Unscramble CS Problems/Task2.py
@@ -3,6 +3,9 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files
 """
 import csv
+from datetime import datetime
+from collections import defaultdict
+
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
@@ -16,7 +19,17 @@ TASK 2: Which telephone number spent the longest time on the phone
 during the period? Don't forget that time spent answering a call is
 also time spent on the phone.
 Print a message:
-"<telephone number> spent the longest time, <total time> seconds, on the phone during 
+"<telephone number> spent the longest time, <total time> seconds, on the phone during
 September 2016.".
 """
+durations = defaultdict(int)
 
+for i in range(0, len(calls) -1):
+    curDate = datetime.strptime(calls[i][2], '%d-%m-%Y %H:%M:%S')
+    if curDate.month == 9 and curDate.year == 2016:
+        durations[calls[i][0]] += int(calls[i][3])
+        durations[calls[i][1]] += int(calls[i][3])
+
+numWithLongestDuration = max(durations, key=durations.get)
+
+print('{} spent the longest time, {} seconds, on the phone during September 2016.'.format(numWithLongestDuration, durations[numWithLongestDuration]))
