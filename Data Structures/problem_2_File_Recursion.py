3a4fb68049ec29f313505d003663893b2d2b3ac4
Ellen Liu <ellen.m.liu@gmail.com>
2020-08-27 22:14:59 -0700

    Updated with solution and test cases

--- Data Structures/problem_2_File_Recursion.py
+++ Data Structures/problem_2_File_Recursion.py
@@ -1,4 +1,5 @@
-#
+import os
+
 def find_files(suffix, path):
     """
     Find all files beneath path with file name suffix.
@@ -15,22 +16,50 @@ def find_files(suffix, path):
     Returns:
        a list of paths
     """
-    paths = list()
-    return paths
+    if not os.path.isdir(path):
+        return "Not a valid path"
 
-find_files()
+    if not suffix or len(suffix) <= 0:
+        return "Not a valid suffix"
 
-# ## Locally save and call this file ex.py ##
-#
-# # Code to demonstrate the use of some of the OS modules in python
-#
-# import os
-#
-# # Let us print the files in the directory in which you are running this script
-# print (os.listdir("."))
-#
-# # Let us check if this file is indeed a file!
-# print (os.path.isfile("./ex.py"))
-#
-# # Does the file end with .py?
-# print ("./ex.py".endswith(".py"))
+    cur_files = os.listdir(path)
+    files = []
+    for file in cur_files:
+        full_path = os.path.join(path, file)
+        if os.path.isdir(full_path):
+            files.extend(find_files(suffix, full_path))
+        if os.path.isfile(full_path):
+            if full_path.endswith(suffix):
+                files.append(full_path)
+
+    return files
+
+test_cases = [
+{
+    "suffix": ".c",
+    "path": "testdir",
+    "expected": ['testdir/subdir3/subsubdir1/b.c', 'testdir/t1.c', 'testdir/subdir5/a.c', 'testdir/subdir1/a.c']
+},
+{
+    "suffix": ".h",
+    "path": "testdir",
+    "expected": ['testdir/subdir3/subsubdir1/b.h', 'testdir/subdir5/a.h', 'testdir/t1.h', 'testdir/subdir1/a.h']
+},
+{
+    "suffix": ".py",
+    "path": "testdir",
+    "expected": []
+},
+{
+    "suffix": "",
+    "path": "testdir",
+    "expected": "Not a valid suffix"
+},
+{
+    "suffix": ".py",
+    "path": "testing",
+    "expected": "Not a valid path"
+}]
+
+for case in test_cases:
+    print("Finding files ending with {} in path {}: ".format(case["suffix"], case["path"]), "pass" if find_files(case["suffix"], case["path"]) == case["expected"] else "fail")
