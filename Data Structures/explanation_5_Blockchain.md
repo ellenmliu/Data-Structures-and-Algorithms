6df1aeea31b014a966f6bc9cf9f078c11c456931
Ellen Liu <ellen.m.liu@gmail.com>
2020-09-01 22:05:25 -0700

    Add solution and explanation for problem 5

---
+++ Data Structures/explanation_5_Blockchain.md
@@ -0,0 +1,1 @@
+The only method that the block chain has is the append method, which takes O(1) time because we are storing the last inserted block. That way we don't have to iterate through the linked list every time we need to append.
--- Data Structures/problem_5_Blockchain.py
+++ Data Structures/problem_5_Blockchain.py
@@ -1,6 +1,14 @@
-import hashlib
+import hashlib, datetime
 
-def calc_hash(self):
+class Block:
+
+    def __init__(self, data, previous_hash=0):
+      self.timestamp = datetime.datetime.now(datetime.timezone.utc).strftime("%H:%M:%S %m/%d/%y ")
+      self.data = data
+      self.previous_hash = previous_hash
+      self.hash = self.calc_hash()
+
+    def calc_hash(self):
       sha = hashlib.sha256()
 
       hash_str = "We are going to encode this string of data!".encode('utf-8')
@@ -9,10 +17,49 @@ def calc_hash(self):
 
       return sha.hexdigest()
 
-class Block:
+    def __repr__(self):
+        s = '\nBlock('
+        s += '\nTimestamp: {}'.format(self.timestamp)
+        s += '\nData: {}'.format(self.data)
+        s += '\nSHA256 Hash: {}'.format(self.hash)
+        s += '\nPrevious Hash: {})'.format(self.previous_hash.hash if self.previous_hash else 0)
+        return s
 
-    def __init__(self, timestamp, data, previous_hash):
-      self.timestamp = timestamp
-      self.data = data
-      self.previous_hash = previous_hash
-      self.hash = self.calc_hash()
+
+class BlockChain:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def size(self):
+        return self.length
+
+    def append(self, value):
+        if self.head == None:
+            new_block = Block(value, None)
+            self.head = new_block
+            self.tail = self.head
+        else:
+            new_block = Block(value, self.tail)
+            self.tail = new_block
+        self.length += 1
+
+    def __repr__(self):
+        blocks = []
+        next = self.tail
+
+        while next:
+            blocks.append(repr(next))
+            next = next.previous_hash
+
+        return "BlockChain({})".format(", ".join(blocks))
+
+block_chain = BlockChain()
+block_chain.append(None)
+print(repr(block_chain))
+block_chain.append(1)
+block_chain.append("Some info")
+block_chain.append(3)
+print(repr(block_chain))
