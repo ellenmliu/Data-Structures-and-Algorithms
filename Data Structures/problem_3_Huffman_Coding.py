0843b6c5ca373476467440484d15b15fa7462287
Ellen Liu <ellen.m.liu@gmail.com>
2020-09-01 23:38:49 -0700

    Started problem 3 with the first step

--- Data Structures/problem_3_Huffman_Coding.py
+++ Data Structures/problem_3_Huffman_Coding.py
@@ -1,6 +1,22 @@
 import sys
+class Node:
+    def __init__(self, char=None, frequency=None, left=None, right=None):
+        self.char = char
+        self.frequency = frequency
+        self.left = left
+        self.right = right
+
+
 
 def huffman_encoding(data):
+    frequency = {}
+
+    for char in data:
+        if char in frequency.keys():
+            frequency[char] += 1
+        else:
+            frequency[char] = 1
+    print(frequency)
     pass
 
 def huffman_decoding(data,tree):
@@ -11,15 +27,15 @@ if __name__ == "__main__":
 
     a_great_sentence = "The bird is the word"
 
-    print ("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
-    print ("The content of the data is: {}\n".format(a_great_sentence))
+    # print ("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
+    # print ("The content of the data is: {}\n".format(a_great_sentence))
 
     encoded_data, tree = huffman_encoding(a_great_sentence)
 
-    print ("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
-    print ("The content of the encoded data is: {}\n".format(encoded_data))
-
-    decoded_data = huffman_decoding(encoded_data, tree)
-
-    print ("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
-    print ("The content of the encoded data is: {}\n".format(decoded_data))
+    # print ("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
+    # print ("The content of the encoded data is: {}\n".format(encoded_data))
+    #
+    # decoded_data = huffman_decoding(encoded_data, tree)
+    #
+    # print ("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
+    # print ("The content of the encoded data is: {}\n".format(decoded_data))
