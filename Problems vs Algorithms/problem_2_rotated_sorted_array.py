7a44c1679c45464796353027c1e3968f752d7a3b
Ellen Liu <ellen.m.liu@gmail.com>
2020-09-14 23:55:12 -0700

    Refactored to reduce space complexity

--- Problems vs Algorithms/problem_2_rotated_sorted_array.py
+++ Problems vs Algorithms/problem_2_rotated_sorted_array.py
@@ -9,31 +9,34 @@ def rotated_array_search(input_list, number):
     """
     if number is None or type(number) != int:
         return -1
-    return rotated_array_searching(input_list, number, 0, len(input_list) - 1)
 
-def rotated_array_searching(input_list, number, start, end):
-    if start > end:
-        return -1
+    start = 0
+    end = len(input_list) - 1
 
-    mid = (start + end) // 2
-    if input_list[mid] == number:
-        return mid
-    if input_list[start] <= input_list[mid]:
-        if number == input_list[start]:
-            return start
-        if number == input_list[mid]:
+    while start <= end:
+        mid = (start + end) // 2
+        if input_list[mid] == number:
             return mid
-        if number > input_list[start] and number < input_list[mid]:
-            return rotated_array_searching(input_list, number, start, mid - 1)
-        return rotated_array_searching(input_list, number, mid + 1, end)
+        if input_list[start] <= input_list[mid]:
+            if number == input_list[start]:
+                return start
+            if number == input_list[mid]:
+                return mid
+            if number > input_list[start] and number < input_list[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+        else:
+            if number == input_list[end]:
+                return end
+            if number == input_list[mid]:
+                return mid
+            if number > input_list[mid] and number < input_list[end]:
+                start = mid + 1
+            else:
+                end = mid - 1
 
-    if number == input_list[end]:
-        return end
-    if number == input_list[mid]:
-        return mid
-    if number > input_list[mid] and number < input_list[end]:
-        return rotated_array_searching(input_list, number, mid + 1, end)
-    return rotated_array_searching(input_list, number, start, mid - 1)
+    return -1
 
 def linear_search(input_list, number):
     for index, element in enumerate(input_list):
